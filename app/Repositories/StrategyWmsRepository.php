c53509555fdca71d6cc4c5bb8b0e2652de64aa60
Bruno Caramelo <bruno.caramelo5@gmail.com>
2024-07-27 03:39:23 -0300

    aplicacao funcionando

--- app/Repositories/StrategyWmsRepository.php
+++ app/Repositories/StrategyWmsRepository.php
@@ -11,27 +11,32 @@ class StrategyWmsRepository implements StrategyWmsInterface
 
     public function findByHourInstant(int $codeStrategy, string $hour, string $instant)
     {
-        return $this->searchData([
+        return $this->searchDataGet([
             'cd_estrategia' => $codeStrategy,
             'range_hour' => "$hour:$instant"
         ]);
     }
 
-    public function searchData(array $filters)
+    public function searchDataGet(array $filters)
     {
-
-        return $this->model::with(['horariosPrioridade' => function($query) use ($filters) {
-                $query->when(!empty($filters['range_hour']), function ($query) use ($filters) {
-                        $hourInt = str_pad($filters['range_hour'], 4, '0', STR_PAD_LEFT);
-
-                        $query->whereRaw("CAST(ds_horario_inicio  AS TIME) <= CAST(?  AS TIME)  AND CAST(ds_horario_final  AS TIME) >= CAST(?  AS TIME) ", ["'$hourInt:00'", "'$hourInt:59'"]);
-
-                    });
-                }])
-                ->when(!empty($filters['cd_estrategia']), function ($query) use ($filters) {
-                    $query->where('cd_estrategia_wms', '=' ,$filters['cd_estrategia']);
-                })
+        return $this->filterBuildData($filters)
                 ->get();
+    }
+
+    public function filterBuildData(array $filters)
+    {
+        return $this->model::with(['horariosPrioridade' => function($query) use ($filters) {
+            $query->when(!empty($filters['range_hour']), function ($query) use ($filters) {
+                    $hourInt = str_pad($filters['range_hour'], 4, '0', STR_PAD_LEFT);
+
+                    $query->whereRaw("CAST(ds_horario_inicio  AS TIME) <= CAST(?  AS TIME)  AND CAST(ds_horario_final  AS TIME) >= CAST(?  AS TIME) ", ["'$hourInt:00'", "'$hourInt:59'"]);
+
+                });
+            }])
+            ->when(!empty($filters['cd_estrategia']), function ($query) use ($filters) {
+                $query->where('cd_estrategia_wms', '=' ,$filters['cd_estrategia']);
+            });
+
     }
 
     public function create(array $data)
